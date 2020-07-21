10e7522516e9a0fa6819a9a0c398f0d733e5c156
AnhTX <anh.truongxuan@elinext.com>
2020-07-21 18:52:36 +0700

    anhtx - update new test

--- src/container/app.jsx
+++ src/container/app.jsx
@@ -8,15 +8,43 @@ class App extends React.Component {
     super(props);
   }
 
+  checkArray() {
+    console.log('here');
+    const array = [
+      'animal/mammal/dog',
+      'animal/mammal/cat/tiger',
+      'animal/mammal/cat/lion',
+      'animal/mammal/elephant',
+      'animal/reptile',
+      'plant/sunflower'
+    ]
+    console.log('array', array);
+    let mainObject = {};
+    array.map((item) => {
+      const newSplit = item.split('/');
+      console.log('split', newSplit);
+      // mainObject = Object.assign(mainObject, newSplit);
+      console.log('mainObject', mainObject);
+
+      // newSplit.map((item) => {
+      //   console.log('item string', item);
+      // })
+      for (const key of newSplit) {
+        mainObject[key] = 'new';
+      }
+    })
+  }
+
   render() {
     return (
       <Router>
         <div className="container">
           <Header headerLeft="Logo company" headerRight="Menu conpany"/>
           <div className="row justify-content-center">
-            <h3 className="mx-5">
+            <h3 className="d-flex w-100 justify-content-center mb-5">
               React App
             </h3>
+            <button onClick={this.checkArray}>Check Array Split</button>
           </div>
           <Footer/>
         </div>
